9194c9c341c7a2c9ed78556e9207e6f42674c125
Acid2510 <kareemacid@gmail.com>
2015-05-24 11:40:18 +0700

    initial commit

---
+++ suff/weeek 4/for.py
@@ -0,0 +1,21 @@
+#print 1 2 3 4 5
+print("#print 1 2 3 4 5")
+for loop in range (1,6):
+    print loop,
+print'\n'
+
+#print 0 2 4 6 8
+print('#print 0 2 4 6 8')
+for loop in range (0,10,2):
+    print loop,
+print'\n'
+
+#print 5 4 3 2 1
+print('#print 5 4 3 2 1')
+for loop in range(5,0,-1):
+    print loop,
+print'\n'
+print('#print 20 15 10 5 0')
+for loop in range (20,-5,-5):
+    print loop,
+print'\n'
