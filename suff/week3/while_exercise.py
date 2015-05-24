2f42e817b1cacfd4cab0ea7145162de75d67b2fe
Acid2510 <kareemacid@gmail.com>
2015-05-24 10:57:22 +0700

    initial commit

--- suff/week3/while_exercise.py
+++ suff/week3/while_exercise.py
@@ -2,5 +2,5 @@ Word=raw_input('Input a word:')
 num=input('how many times:')
 loop=0
 while loop<num:
-    print ('{0} lo{1}'.format(Word,loop+1))
+    print ('{0} {1}'.format(Word,loop+1))
     loop=loop+1
