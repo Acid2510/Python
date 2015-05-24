ad9e59ea0e495880ae4a25f2fae14b108a14d038
Acid2510 <kareemacid@gmail.com>
2015-05-24 11:13:57 +0700

    initial commit

--- suff/weeek 4/exercise_close.py
+++ suff/weeek 4/exercise_close.py
@@ -7,14 +7,17 @@ while close !='no':
     print('3.calculate area of square')
     number=input('Your choice(1-3):')
     if number==1:
+        print('calculating the are of triangle')
         base=input('what is the length of the base?:')
         height=input('what is the length of the height?:')
         print('the area of the triagle is:{0}'.format(base*height/2))
     if number==2:
-              lenght=input('what is the length of the rectangle:')
-              width=input('what is the width of the rectangle:')
-              print ('the area of the square is:{0}'.format(lenght*width))
+        print('calculating the are of rectangle')
+        lenght=input('what is the length of the rectangle:')
+        width=input('what is the width of the rectangle:')
+        print ('the area of the square is:{0}'.format(lenght*width))
     if number==3:
-              side=input('what is the side of the square:')
-              print ('the area of the square is:{0}'.format(side*side))
+        print('calculating the are of square')
+        side=input('what is the side of the square:')
+        print ('the area of the square is:{0}'.format(side*side))
     close=raw_input('want to find another area?(yes/no):')
