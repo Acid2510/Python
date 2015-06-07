8ab46f6d162b08b2f86cd2bc058a99172e960c6b
Acid2510 <kareemacid@gmail.com>
2015-06-07 11:12:27 +0700

    commit week 5

---
+++ suff/Week 5/For_exercise.py
@@ -0,0 +1,26 @@
+#print word that user input 5 times use 'for'word
+word=raw_input('word:')
+for loop in range(0,5):
+    print ('{0} {1}'.format(word,loop+1))
+
+ #print word that user input 5 times use 'for'word
+word=raw_input('word:')
+for loop in range(6,1,-1):
+    print ('{0} {1}'.format(word,loop-1))
+    
+#print word that user input 5 times use 'for'word
+word=raw_input('word:')
+for loop in range(20,-1,-5):
+    print ('{0} {1}'.format(word,loop))
+    
+    
+#print word that user input 5 times use 'for'
+word=raw_input('word:')
+for loop in range(0,25,5):
+    print ('{0} {1}'.format(word,loop))
+
+word=raw_input('Word:')
+many=input('how many times:')
+for loop in range(1,many+1):
+    print ('{0} {1}'.format(word,loop))
+
